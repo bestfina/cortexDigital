25dd48eb23561a2f373a801a28ac2399305b54a7
German Vl <tertydert@gmail.com>
2024-11-15 07:45:56 +0300

    remove-about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Hero from "@/components/sections/Hero";
 import Portfolio from "@/components/sections/Portfolio";
-import About from "@/components/sections/About";
 import Service from "@/components/sections/Service";
 
 const Home = () => (
   <>
     <Hero />
     <Portfolio />
-    <About />
     <Service />
   </>
 );
