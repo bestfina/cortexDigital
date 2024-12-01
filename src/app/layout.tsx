fd86a65d219c144a275e6c624bd977e9d2fb25a4
German Vl <tertydert@gmail.com>
2024-12-02 01:53:21 +0300

    metrica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { PopupProvider } from "@/context/PopupContext";
 import Header from "@/components/sections/Header";
 import Footer from "@/components/sections/Footer";
 import Popup from "@/components/ui/Popup";
+import YandexMetrika from "@/components/sections/YandexMetrika";
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -35,30 +36,7 @@ const RootLayout = ({
   return (
     <html lang="ru">
       <head>
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(m,e,t,r,i,k,a){
-                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                  m[i].l=1*new Date();
-                  k=e.createElement(t),a=e.getElementsByTagName(t)[0];
-                  k.async=1;k.src=r;a.parentNode.insertBefore(k,a)
-              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-              ym(12345678, "init", {
-                  clickmap:true,
-                  trackLinks:true,
-                  accurateTrackBounce:true
-              });
-            `,
-          }}
-        ></script>
-        <noscript>
-          <div>
-            <img src="https://mc.yandex.ru/watch/12345678" style={{ position: "absolute", left: "-9999px" }} alt="" />
-          </div>
-        </noscript>
+        <YandexMetrika />
         <meta name="yandex-verification" content="8cf8659d30ac0744" />
       </head>
       <body className={twMerge(raleway.className, pressStart2P.variable, "antialiased text-TextDark")}>
