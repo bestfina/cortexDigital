3c34b2b7c7fd34485711fd3c1189607e76e78619
German Vl <tertydert@gmail.com>
2025-01-09 11:03:47 +0300

    config fix header

--- next.config.mjs
+++ next.config.mjs
@@ -8,24 +8,6 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 60,
   },
-  compiler: {
-    removeConsole: {
-      exclude: ["error"],
-    },
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
